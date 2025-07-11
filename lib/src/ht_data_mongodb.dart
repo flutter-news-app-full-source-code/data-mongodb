cb7dd34bca998933e1fd08f0916f3ececd068cfb
fulleni <fulleni@hotmail.com>
2025-07-11 08:56:28 +0100

    chore: init

---
+++ lib/src/ht_data_mongodb.dart
@@ -0,0 +1,7 @@
+/// {@template ht_data_mongodb}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class HtDataMongodb {
+  /// {@macro ht_data_mongodb}
+  const HtDataMongodb();
+}
---
+++ test/src/ht_data_mongodb_test.dart
@@ -0,0 +1,12 @@
+// Not required for test files
+// ignore_for_file: prefer_const_constructors
+import 'package:ht_data_mongodb/ht_data_mongodb.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('HtDataMongodb', () {
+    test('can be instantiated', () {
+      expect(HtDataMongodb(), isNotNull);
+    });
+  });
+}
