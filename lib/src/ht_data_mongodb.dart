bc34d95ca01bf3a63c62b10b7ae2185a3228d775
fulleni <fulleni@hotmail.com>
2025-07-11 09:22:55 +0100

    feat: create HtDataMongodb client class structure
    Replaces the placeholder class with a structured `HtDataMongodb<T>`
    class that implements the `HtDataClient<T>` interface.
    
    - Sets up the constructor to accept dependencies: `MongoDbConnectionManager`,
      `modelName`, `FromJson`, `ToJson`, and a `Logger`.
    - Adds stubs for all required data client methods.

--- lib/src/ht_data_mongodb.dart
+++ lib/src/ht_data_mongodb.dart
@@ -1,7 +1,74 @@
+import 'package:ht_data_client/ht_data_client.dart';
+import 'package:ht_data_mongodb/src/mongo_db_connection_manager.dart';
+import 'package:ht_shared/ht_shared.dart';
+import 'package:logging/logging.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
 /// {@template ht_data_mongodb}
-/// A Very Good Project created by Very Good CLI.
+/// A MongoDB implementation of the [HtDataClient] interface.
+///
+/// This client interacts with a MongoDB database to perform CRUD operations,
+/// translating the generic data client requests into native MongoDB queries.
 /// {@endtemplate}
-class HtDataMongodb {
+class HtDataMongodb<T> implements HtDataClient<T> {
   /// {@macro ht_data_mongodb}
-  const HtDataMongodb();
+  HtDataMongodb({
+    required MongoDbConnectionManager connectionManager,
+    required String modelName,
+    required FromJson<T> fromJson,
+    required ToJson<T> toJson,
+    Logger? logger,
+  })  : _connectionManager = connectionManager,
+        _modelName = modelName,
+        _fromJson = fromJson,
+        _toJson = toJson,
+        _logger = logger ?? Logger('HtDataMongodb<$T>');
+
+  final MongoDbConnectionManager _connectionManager;
+  final String _modelName;
+  final FromJson<T> _fromJson;
+  final ToJson<T> _toJson;
+  final Logger _logger;
+
+  /// A getter for the MongoDB collection for the given model type [T].
+  DbCollection get _collection => _connectionManager.db.collection(_modelName);
+
+  @override
+  Future<SuccessApiResponse<T>> create({required T item, String? userId}) {
+    // TODO: implement create
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> delete({required String id, String? userId}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<SuccessApiResponse<T>> read({required String id, String? userId}) {
+    // TODO: implement read
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<SuccessApiResponse<PaginatedResponse<T>>> readAll({
+    String? userId,
+    Map<String, dynamic>? filter,
+    PaginationOptions? pagination,
+    List<SortOption>? sort,
+  }) {
+    // TODO: implement readAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<SuccessApiResponse<T>> update({
+    required String id,
+    required T item,
+    String? userId,
+  }) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
 }
